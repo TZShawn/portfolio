d78f4ed332fcba4c0caae2fc59bd54439e2957a1
Shawn Xue <shawn67973522@gmail.com>
2023-01-19 18:22:30 -0500

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import pnglogo from './Images/pnglogo.png';
 import './App.css';
+import Landing from './Sections/landing';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='h-screen w-screen overflow-hidden'>
+      <Navbar>
+        <Container className='m-4'>
+          <Navbar.Brand href="#home">
+            <img
+              src={pnglogo}
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+              alt="logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <div className='h-full w-full flex-col'>
+        <Landing />
+      </div>
+    </div>
   )
 }
--- src/Sections/landing.tsx
+++ src/Sections/landing.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Landing: React.FC<{}> = () => {
-    return <div></div>
-}
+  return (
+    <div className="flex-1 w-full h-full m-auto">
+      <div className="h-full w-1/2 m-16">
+        <div className="font-bold text-slate-400 text-3xl">Hi!</div>
+        <div className="font-bold text-black text-6xl">I'm Shawn Xue</div>
+        <div className="font-bold text-black text-3xl">I am a Mathematics student at The University of Waterloo and an aspiring Web Developer and Software Engineer</div>
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
