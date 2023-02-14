0074e893f006f68ace7bb55e64172b401dbc13eb
Shawn Xue <shawn67973522@gmail.com>
2023-02-14 15:27:00 -0500

    commit

--- src/Sections/skills.tsx
+++ src/Sections/skills.tsx
@@ -4,8 +4,8 @@ const Skills: React.FC<{}> = ({}) => {
   return (
     <div className="mx-auto px-32 my-14">
       <div className="font-bold text-black text-4xl py-4">My Skills</div>
-      <div>
-        
+      <div className="flex">
+        <SkillBox name={"C"} image={"../Images/pnglogo.png"}/>
       </div>
     </div>
   );
@@ -14,7 +14,7 @@ const Skills: React.FC<{}> = ({}) => {
 const SkillBox: React.FC<{name: string, image: string}> = ({name, image}) => {
   return (
     <div>
-      <img src={`../Images/${image}.png`} />
+      <img src={image} />
       <div>{name}</div>
     </div>
   )
